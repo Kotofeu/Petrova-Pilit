9b5000ec36c05e9cc1ce5f93b6752bf557f2f2aa
Petrov Danil <cras.petrov@yandex.ru>
2024-09-08 08:28:28 +0200

    Реализация hover эфекта карточки работы

--- src/modules/Works/WorksComponent/WorkCard/WorkCard.tsx
+++ src/modules/Works/WorksComponent/WorkCard/WorkCard.tsx
@@ -1,9 +1,10 @@
-import { FC, memo, useCallback } from 'react';
+import { FC, memo, useCallback, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import classes from './WorkCard.module.scss';
 import classConnection from '../../../../utils/function/classConnection';
 import { WORKS_ROUTE } from '../../../../utils/const/routes';
+import Button from '../../../../UI/Button/Button';
 
 interface IWorkCard {
     id: number;
@@ -14,24 +15,26 @@ interface IWorkCard {
 
 const WorkCard: FC<IWorkCard> = memo(({ id, className, title, image }) => {
     const router = useNavigate();
-    
-    const onCardClick = useCallback((event: React.MouseEvent<HTMLDivElement>) => {
-        event.preventDefault();
+    const [isShowMore, setIsShowMore] = useState<boolean>(false)
+
+    const onCardClick = useCallback(() => {
         router(`${WORKS_ROUTE}/${id}`);
     }, [id]);
 
     if (!image) return null;
 
     return (
-        <motion.article 
-            className={classConnection(classes.workCard, className)} 
-            onClick={onCardClick}
-            layout // Позволяет анимировать изменения в расположении элементов
-            initial={{ opacity: 0, scale: 0.9 }} 
-            exit={{ opacity: 0, scale: 0.9 }} 
-            animate={{ opacity: 1, scale: 1 }} 
+        <motion.article
+            className={classConnection(classes.workCard, isShowMore ? classes.workCard_active : '', className)}
+            layout
+            initial={{ opacity: 0, scale: 0.9 }}
+            exit={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
         >
             <img className={classes.workCard__image} src={image} alt={title} />
+            <h4 className={classes.workCard__title}>{title}</h4>
+            <Button onClick={onCardClick} className={classes.workCard__button}>Посмотреть подробнее</Button>
+            <Button onClick={() => setIsShowMore(prev => !prev)} className={classes.workCard__show} />
         </motion.article>
     );
 });
